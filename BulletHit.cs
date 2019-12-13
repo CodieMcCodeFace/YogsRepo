a460d8261996c01ce3cb859eaa159fe0517eef9b
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2019-12-13 15:48:04 -0800

    Placeholder Shooting Mechanics
    Make sure to allow the bullet to deal some sort of damage.

---
+++ BulletHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Subtract HP and add a bullet explosion
+        if (collision.tag == "wall")
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.tag == "enemy")
+        {
+            Debug.Log("Enemy Hit!");
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public Transform firePoint;
+    public float bulletSpeed = 5.0f;
+    void Update()
+    {
+        ShootInput();
+    }
+
+    void ShootInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            BulletShot();
+        }
+    }
+    void BulletShot()
+    {
+        GameObject bullet = Instantiate(prefab, firePoint.position, Quaternion.identity);
+
+        Rigidbody2D Rigid = bullet.GetComponent<Rigidbody2D>();
+
+        Rigid.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+
+    }
+}

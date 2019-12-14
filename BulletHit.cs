7f28408301b0c706e87711fd81425d6166765d43
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2019-12-14 08:37:37 -0800

    Quick Update for Scripts
    Added BulletShot prefab which is the bullet. Attach BulletHit.cs to BulletShot.prefab
    
    Added Shotgun/Triple Shot, the player must have 3 Firepoints and attach Shoot.cs to Player

--- BulletHit.cs
+++ BulletHit.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class BulletHit : MonoBehaviour
 {
-
+    public GameObject bulletEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Subtract HP and add a bullet explosion
         if (collision.tag == "wall")
         {
+            GameObject wallHit = Instantiate(bulletEffect, gameObject.transform.position, Quaternion.identity);
+
+            Destroy(wallHit, 0.5f);
             Destroy(gameObject);
         }
         else if(collision.tag == "enemy")
         {
             Debug.Log("Enemy Hit!");
+            GameObject effect = Instantiate(bulletEffect,transform.position, Quaternion.identity);
+
+            Destroy(effect, 0.5f);
             Destroy(gameObject);
         }
     }
--- Shoot.cs
+++ Shoot.cs
@@ -7,7 +7,8 @@ public class Shoot : MonoBehaviour
 
     public GameObject prefab;
     public Transform firePoint;
-    public float bulletSpeed = 5.0f;
+    public float bulletSpeed = 15f;
+    public float shotgunBulletSpeed = 15.0f;
     void Update()
     {
         ShootInput();
@@ -28,5 +29,25 @@ public class Shoot : MonoBehaviour
 
         Rigid.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
 
+    }
+    
+    void shotgunShot()
+    {
+
+        GameObject shotgun = Instantiate(prefab, firePoint.position, Quaternion.identity);
+
+        Vector3 shotgunPos = new Vector3(0, 0.5f, 0);
+        Vector3 shotgunPos2 = new Vector3(0f, 0.5f, 0f);
+
+        GameObject shot2 = Instantiate(prefab, firePoint.position + shotgunPos, Quaternion.identity);
+        GameObject shot3 = Instantiate(prefab, firePoint.position - shotgunPos, Quaternion.identity);
+
+        Rigidbody2D shotgunPhysics = shotgun.GetComponent<Rigidbody2D>();
+        Rigidbody2D shot2Phys = shot2.GetComponent<Rigidbody2D>();
+        Rigidbody2D shot3Phys = shot3.GetComponent<Rigidbody2D>();
+
+        shot2Phys.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+        shot3Phys.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+        shotgunPhysics.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
     }
 }

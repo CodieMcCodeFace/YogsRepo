93ca64adef7bde8069cc8588a198655b06cbfe54
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2019-12-13 16:31:10 -0800

    Player Controller by Brandon
    Added mouse follow on the player

---
+++ PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public float moveSpeed = 5.0f;
+    public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 movement;
+    Vector2 mousePosition;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rb.position;
+
+        float atanAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+        rb.rotation = atanAngle;
+    }
+
+}
